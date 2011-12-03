ea9a3a9c9cf31ec18158ae1ccb9c949244e7764a
Armin Ronacher <armin.ronacher@active-4.com>
2011-12-03 18:03:20 -0500

    For loop is now with parent access by default in templatetk

--- jsonjinja/config.py
+++ jsonjinja/config.py
@@ -1,13 +1,5 @@
 from templatetk.config import Config as ConfigBase, Undefined
-from templatetk.runtime import LoopContext as LoopContextBase
 from weakref import ref as weakref
-
-
-class LoopContext(LoopContextBase):
-
-    def __init__(self, iterator, parent):
-        LoopContextBase.__init__(self, iterator)
-        self.parent = parent
 
 
 class Config(ConfigBase):
@@ -16,9 +8,6 @@ class Config(ConfigBase):
         ConfigBase.__init__(self)
         self._environment = weakref(environment)
         self.forloop_parent_access = True
-
-    def wrap_loop(self, iterator, parent=None):
-        return LoopContext(iterator, parent)
 
     @property
     def environment(self):
